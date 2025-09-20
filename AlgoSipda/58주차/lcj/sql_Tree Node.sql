c55f09721545d64d1338ca53d769f6a20fd91d16
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2025-09-20 20:04:31 +0900

    [58주차] 이채정 문제 풀이

---
+++ AlgoSipda/58주차/lcj/sql_Tree Node.sql
@@ -0,0 +1,10 @@
+select
+    id,
+    (
+        case 
+            when p_id is null then 'Root'
+            when id in (select p_id from Tree) then 'Inner'
+            else 'Leaf'
+        end
+    ) 'type'
+from tree;
